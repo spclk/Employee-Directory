edf546beebe784a842b9bac0e3b16bec7d0d41a3
Konstantin <konstantin1020@gmail.com>
2021-05-10 20:37:32 -0700

    css styling

--- src/components/EmployeeListItem/index.js
+++ src/components/EmployeeListItem/index.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./style.css";
 
 function EmployeeListItem(props) {
   return (
-    <div className="card mb-3" style={{ maxWidth: "800px" }}>
+    <div className="card mb-3" style={{ maxWidth: "700px", height: "180px"}}>
       <div className="row g-0">
-        <div className="col-md-4">
+        <div className="col-md-4 image">
           <img src={props.employee.picture.large} alt="..." />
         </div>
-        <div className="col-md-8">
+        <div className="col-md-6">
           <div className="card-body">
             <h5 className="card-title">{props.employee.name.first} {props.employee.name.last}</h5>
             <p className="card-text"> Email: {props.employee.email}</p>
