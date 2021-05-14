06d8e37f54e5bfc83b249606a1c49d0e14c3d53a
Konstantin <konstantin1020@gmail.com>
2021-05-13 22:01:46 -0700

    modified navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,7 +7,7 @@ function Navbar(props) {
         <div className="container">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
-            <a className="navbar-brand" href="/">Employee Directory</a>
+            <h4 className="navbar-brand">Employee Directory</h4>
             <form className="d-flex">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"
             name = "search"
